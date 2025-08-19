36865c61036dc5476bd311ed84d40986b347eff4
sonu12091995 <sonu735700@gmail.com>
2025-08-19 18:38:45 +0530

    DryRun

---
+++ src/test/java/org/Selenium/Dry_runTest.java
@@ -0,0 +1,12 @@
+package org.Selenium;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Dry_runTest {
+    @Test
+    public void Demotest(){
+        System.out.println("Checking setup");
+        Assert.assertEquals("Sonu","Sonu");
+    }
+}
